09541361c73574118facd86f23ca626268138fd4
ritty025 <ritikapradhan025@gmail.com>
2025-06-30 14:16:33 +0530

    requirements added

--- template.py
+++ template.py
@@ -22,7 +22,8 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 
     ]
 
@@ -33,7 +34,7 @@ for filepath in list_of_files:
     if filedir != "":
         os.makedirs(filedir , exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
-    if (not os.path.exists(filepath)) or (os.apth.getsize(filepath) == 0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,"w") as f:
             pass
         logging.info(f"Creating empty file: {filepath}")
